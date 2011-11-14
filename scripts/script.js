ed2e2036727177fde11d669e6edc2b73f951c348
Zeno Rocha <contato@zenorocha.com>
2011-11-14 15:48:37 -0200

    Well done, now I'm listing my repositories, but it doesn't seems to be good to my case

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
 	(function($) {
 		
-		var json = [],
+		var repos = [],
 			username = 'zenorocha';
 		
 		$.getJSON('https://api.github.com/users/' + username + '/repos', function(result){
 	    	
 			$.each(result, function(i, field) {
 	    		if (field.language != null)
-	    			json.push(field);
+	    			repos.push(field);
 			});
 
-			json.sort(function(a, b) {
+			repos.sort(function(a, b) {
 				
 				var langA = a.language, 
 					langB = b.language;
@@ -22,10 +22,45 @@
 
 			});
 
-			console.log(json);
+			$.each(repos, function(i, field) {
+	    		
+	    		if (i > 0) {
+					if (repos[i].language != repos[i-1].language) {
+		    			createCategory(repos[i].language);
+		    		}	    			
+	    		}
+	    		else {
+	    			createCategory(repos[i].language);
+	    		}
+
+	    		createRepo(repos[i]);
+
+			});
 
 		});
 
-		
+		var createCategory = function(catName) {
+			
+			var cat = '<div class="category">' +
+							'<h2>' + catName + '</h2>' +
+					   '</div>';
+
+			$('body').append(cat);
+
+		}
+
+		var createRepo = function(repo) {
+			
+			var repository = '<a href="' + repo.html_url + '" class="title">' +
+								'<div>' +
+									'<h1>' + repo.name + '</h1>' +
+									'<p>' + repo.description + '</p>' +
+									'<small>' + repo.homepage + '</small>' +
+								'</div>' +
+							'</a>';	
+
+			$('body').append(repository);
+			
+		}
 		
 	})(jQuery);
